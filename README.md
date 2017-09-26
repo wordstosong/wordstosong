ab8bb7fcc52cc910a3dae498b5562cc6d674c792
wordstosong <johnja14@gmail.com>
2017-09-25 21:12:11 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# JacobScript
+
+This is the repository for the site [**Speak Louder!**][1]
+
+## Installation
+
+1. Install [Node 8](https://nodejs.org/en/)
+
+    If you need to manage multiple versions of Node, please install [NVM][2] to 
+    manage multiple versions of Node and [AVN][3] to automatically switch between versions.
+
+2. Run the following commands:
+
+    ```bash
+    npm install
+    npm start
+    ```
+
+[1]: http://www.speak-louder.com/
+[2]: https://github.com/creationix/nvm
+[3]: https://github.com/wbyoung/avn
---
+++ app.js
@@ -0,0 +1,55 @@
+var chalk = require('chalk');
+var exphbs  = require('express-handlebars');
+var expressServer = require('express');
+var openBrowser = require('./tools/openBrowser');
+
+var isDev = process.env.NODE_ENV === 'development';
+var app = expressServer();
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: './views/layouts',
+    partialsDir: './views'
+}));
+app.set('view engine', 'handlebars');
+
+var title = 'Speak Louder!';
+
+app.get('/', function (req, res) {
+    res.render('home', { title: title });
+});
+
+app.get('/about', function (req, res) {
+    res.render('about', { title: title + ' | About' });
+});
+
+app.get('/archive', function (req, res) {
+    res.render('archive', { title: title + ' | Archive' });
+});
+
+app.get('/cast', function (req, res) {
+    res.render('cast', { title: title + ' | Cast' });
+});
+
+app.use(expressServer.static('public'));
+
+var defaultPort = 8081;
+
+var port = process.env.PORT || defaultPort;
+app.listen(port, function() {
+    console.log(chalk.blue('server is listening on port ' + port));
+
+    if (isDev) {
+        var browserSync = require('browser-sync');
+        var bsPort = port + 1;
+        browserSync({
+            files: ['public/**/*.{html,js,css}', 'views/**/*.handlebars'],
+            online: false,
+            open: false,
+            port: bsPort,
+            proxy: 'localhost:' + port,
+            ui: false
+        });
+        openBrowser(bsPort);
+    }
+});
