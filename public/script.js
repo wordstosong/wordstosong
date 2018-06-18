af26bd4bc7361e8445c57428530fb5fb7131f1d9
Jacob Johnson <JacobJohnson@Jacobs-MacBook-Pro.local>
2018-06-18 02:37:36 -0700

    new drop down

--- public/script.js
+++ public/script.js
@@ -111,6 +111,24 @@ AOS.init({
   duration: 2400,
 });
 
+//PORT MENUS
+
+$('.toggle').click(function(e) {
+  e.preventDefault();
+
+  var $this = $(this);
+
+  if ($this.next().hasClass('show')) {
+      $this.next().removeClass('show');
+      $this.next().slideUp(350);
+  } else {
+      $this.parent().parent().find('li .inner').removeClass('show');
+      $this.parent().parent().find('li .inner').slideUp(350);
+      $this.next().toggleClass('show');
+      $this.next().slideToggle(350);
+  }
+});
+
 //Portfolio
 
 function devMenu() {
