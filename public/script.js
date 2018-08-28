60b42a3d5d341a99d0baa99b3212aa338cc64c9f
Jacob Johnson <JacobJohnson@Jacobs-MacBook-Pro.local>
2018-08-28 01:12:52 -0700

    v3 update (icons and copy change)

--- public/script.js
+++ public/script.js
@@ -68,64 +68,46 @@ function parallax(){
 }
   
 
-//TEXT ANIMATION
+//ROTATING TEXT
 
-var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
-};
 
-TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
+(function($){
+  $.fn.extend({ 
+      rotaterator: function(options) {
 
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
+          var defaults = {
+              fadeSpeed: 500,
+              pauseSpeed: 6000,
+      child:null
+          };
+           
+          var options = $.extend(defaults, options);
+       
+          return this.each(function() {
+                var o =options;
+                var obj = $(this);                
+                var items = $(obj.children(), obj);
+        items.each(function() {$(this).hide();})
+        if(!o.child){var next = $(obj).children(':first');
+        }else{var next = o.child;
+        }
+        $(next).fadeIn(o.fadeSpeed, function() {
+          $(next).delay(o.pauseSpeed).fadeOut(o.fadeSpeed, function() {
+            var next = $(this).next();
+            if (next.length == 0){
+                next = $(obj).children(':first');
+            }
+            $(obj).rotaterator({child : next, fadeSpeed : o.fadeSpeed, pauseSpeed : o.pauseSpeed});
+          })
+        });
+          });
+      }
+  });
+})(jQuery);
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-  var that = this;
-  var delta = 300 - Math.random() * 100;
-
-  if (this.isDeleting) { delta /= 2; }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 100;
-  }
-
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
-
-window.onload = function() {
-  var elements = document.getElementsByClassName('txt-rotate');
-  for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
-    var period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
-    }
-  }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-  document.body.appendChild(css);
-};
+$(document).ready(function() {
+      $('#rotate').rotaterator({fadeSpeed:500, pauseSpeed:2000});
+});
 
 //ANIMATE ON SCROLL
 
